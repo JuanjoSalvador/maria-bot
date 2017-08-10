02ecf5e5582b89c9d901e7977e760dd46df06558
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-08-10 19:26:36 +0200

    added repo command

--- src/mariabot.py
+++ src/mariabot.py
@@ -4,7 +4,11 @@ import telebot
 
 bot = telebot.TeleBot(<TOKEN>)
 
-@bot.message_handler(func=lambda m: True, content_types=['new_chat_members', 'text'])
+@bot.message_handler(commands=['repo'])
+def repository(message):
+    bot.reply_to("Repositorio de Maria disponible en GitHub https://github.com/JuanjoSalvador/maria-bot")
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
 def welcome(message):
     new_member = message.new_chat_member.username
     msg = <YOUR WELCOME MESSAGE HERE>
